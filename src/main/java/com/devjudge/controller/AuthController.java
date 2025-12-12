dd71fa384c2f90a605df379cfcf7a84c8e49714e
MeAditya239 <adityaspatil239@gmail.com>
2025-12-12 23:30:46 +0530

    Test change for CodeRabbit PR

--- src/main/java/com/devjudge/controller/AuthController.java
+++ src/main/java/com/devjudge/controller/AuthController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 
+// CodeRabbit test review comment
+
+
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
