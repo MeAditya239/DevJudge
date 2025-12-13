2c2ecf962880f321225ebbeb3e2d92dfd9608489
MeAditya239 <adityaspatil239@gmail.com>
2025-12-13 10:41:47 +0530

    Refactor CodingProblemController to use ResponseEntity as suggested by CodeRabbit

--- src/main/java/com/devjudge/controller/CodingProblemController.java
+++ src/main/java/com/devjudge/controller/CodingProblemController.java
@@ -1,20 +1,24 @@
 package com.devjudge.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/problems")
+@RequestMapping(value="/api/problems", produces="application/json")
 public class CodingProblemController {
+    // Placeholder endpoint – will be implemented in next iteration
     @GetMapping
-    public String getAllProblems() {
-        return "Fetch ALl coding problems - placeholder";
+    public ResponseEntity<String> getAllProblems() {
+        return ResponseEntity.ok("Fetch aLl coding problems - (to be implemented)");
     }
 
-    @PostMapping
-    public String createProblem() {
-        return "Create Coding problem - placeholder";
+    // Placeholder endpoint – will be implemented in next iteration
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<String> createProblem() {
+        return ResponseEntity.status(201)
+                .body("Create coding problem (to be implemented)");
     }
 }
